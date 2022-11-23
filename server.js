0072ed784bcdd50fdba1f91aa0e9e65978fd9c30
dnstrach <dnstrach@gmail.com>
2022-11-23 15:57:31 -0800

    added alert

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(express.json())
+app.use(cors())
 
 app.use(express.static(`${__dirname}/public`))
+
+app.get('/api/aloha', (req, res) => {
+    res.status(200).send('Aloha')
+})
 
 app.listen(4000,
    () => console.log(`server running on 4000`)
